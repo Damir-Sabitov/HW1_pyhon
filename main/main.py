2978c1c0705d9e292cf0ee67a87c1639c2b6d7ea
Damir Sabitov <damirsabitov1998@gmail.com>
2025-07-15 00:30:29 +0300

    dfdfsdfdsfd

--- main/main.py
+++ main/main.py
@@ -2,5 +2,5 @@ from selene import browser, be, have
 
 
 browser.open('https://duckduckgo.com')
-browser.element('[name="q"]').should(be.blank).type('рррррррррвыароврыаовыаолывмиорвалимлроваиморвиаромиваимлваимливаимлравимрвыаимрва').press_enter()
+browser.element('[name="q"]').should(be.blank).type('рррррррррвbbnnnjjыароврыаовыаолывмиорвалимлроваиморвиаромиваимлваимливаимлравимрвыаимрва').press_enter()
 browser.element('html').should(have.text('все нашwffdff!'))
